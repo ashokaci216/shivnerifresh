8b2d8b19ca950cd48275116f20f44559a193e858
ashokaci216 <daya0134@gmail.com>
2025-04-05 00:19:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ function addToCart(name, price) {
   if (qty > 0) {
     cart[name] = cart[name] || { price, quantity: 0 };
     cart[name].quantity += qty;
-    document.getElementById(`qty-${name}`).innerText = 0;
+    document.getElementById(`qty-${name}`).innerText = cart[name].quantity;
     updateCartDisplay();
   }
 }
